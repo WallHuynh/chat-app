3433ba289e9d147e42e791e7cab96658d412503a
wall huynh <phuctuong0862@gmail.com>
2022-07-04 13:12:12 +0700

    update react 18.1.0

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = createRoot(document.getElementById('root'))
 root.render(<App />)
 
 // If you want to start measuring performance in your app, pass a function
